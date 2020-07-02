633bee5281f0a8ea7343ab179c27f21326c5a1c7
mashudranagithub <mashudranabitm@gmail.com>
2020-07-03 02:09:29 +0600

    Menu fixed and vision and mission layout complete

--- vision-mission.php
+++ vision-mission.php
@@ -4,24 +4,12 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6">
-				<div class="vision-image">
-					<img src="assets/img/about/vision.jpg" alt="Vision and Mission Image">
-				</div>
-			</div>
-			<div class="col-md-6">
 				<h3>Vision</h3>
 				<p>To become a center of excellence for providing services through systematic research, innovation and entrepreneurship development, advisory services, databases, information and communication technology and training to the extensive group of national and international organizations for purposes of enhanced effectiveness and efficiency in planning, implementation and monitoring of projects, programs and entrepreneurship development.</p>
 			</div>
-		</div>
-		<div class="row">
 			<div class="col-md-6">
 				<h3>Mission</h3>
 				<p>To provide services for improving management of natural resources, environment, climate change and health for sustainable socioeconomic development at home and abroad, using situation analysis, baseline and impact evaluation including EIA, environmental exposure and risk assessment, database, information technology and entrepreneurship development.</p>
-			</div>
-			<div class="col-md-6">
-				<div class="vision-image">
-					<img src="assets/img/about/mission.jpg" alt="Vision and Mission Image">
-				</div>
 			</div>
 		</div>
 	</div>
